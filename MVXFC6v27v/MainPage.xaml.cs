6965a2a3b21c7d24aeaba91f014b24a126a70445
Ed <pushpoped@gmail.com>
2022-05-16 19:52:05 -0400

    first commit

---
+++ MVXFC6v27v/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVXFC6v27v
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVXFC6v27v/VideModels/FirstViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MVXFC6v27v.CORE.VideModels
+{
+    public class FirstViewModel : ContentPage
+    {
+        public FirstViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
